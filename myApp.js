d6190c46a8b64f9f620f2e139f34fa4e92bc2608
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-17 15:41:26 -0300

    Fixing.

--- myApp.js
+++ myApp.js
@@ -3,15 +3,15 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/views/index.html");
-});
 app.get("/json", function(req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
   res.json({"message": message});
+});
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 
