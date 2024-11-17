249b53ca01d074dead69544bce75ccff6a7bafe9
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-17 20:13:28 -0300

    Delete console_log line.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
