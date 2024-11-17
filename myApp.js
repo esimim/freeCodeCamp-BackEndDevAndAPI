522af76a39ba62aa7b8e13ce1ded889263ad8f9f
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-17 17:00:14 -0300

    Return Hello world instead.

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", function(req, res) {
-  let message = "Hello json";
+  let message = "Hello world";
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
     console.log(message);
