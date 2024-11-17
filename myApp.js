e9f3214ffdcecb26d7d7dceb19732a6d77b1aad6
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-17 15:51:22 -0300

    Trying to fix the add of the _env file.

--- myApp.js
+++ myApp.js
@@ -7,7 +7,9 @@ app.get("/json", function(req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
+    console.log(message);
   }
+  console.log({"message": message});
   res.json({"message": message});
 });
 app.get("/", function(req, res){
