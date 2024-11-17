cbdef10f7219ea6ef4c3b17deac6db1733b08f7f
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-17 19:31:28 -0300

    Add the _env file to the Docker file.

--- myApp.js
+++ myApp.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-console.log("Hello World");
+// console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", function(req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE == 'uppercase') {
     message = message.toUpperCase();
-    console.log(message);
   }
-  console.log({"message": message});
   res.json({"message": message});
 });
 app.get("/", function(req, res){
