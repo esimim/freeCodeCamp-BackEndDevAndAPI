86e4732188fb781ff5adf7712bd719cd9f2dc1a4
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-18 08:32:29 -0300

    Testing a Hello World in a different place.

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World01");
 app.get("/json", function(req, res) {
+  console.log("Hello World02");
+  res.json({"message": "Hello json"});
+});
+app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
     console.log("Hello World02a");
     res.json({message: "HELLO JSON"});
