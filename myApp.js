4258d8fdf8b70472386c7ad17c28dbf5316addf8
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-17 21:34:47 -0300

    Change logic.

--- myApp.js
+++ myApp.js
@@ -5,8 +5,10 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json({"message": "HELLO JSON"});
-  }
-  res.json({"message": "Hello json"});
+  } else {
+    res.json({"message": "Hello json"});
+  };
+  
 });
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html");
