79f86c9f0d77204858077232c15ddfba2b33188a
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-18 08:05:11 -0300

    Trying to set route for verb GET on path /json.

--- myApp.js
+++ myApp.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-app.use("/public", express.static(__dirname + "/public"));
+console.log("Hello World01");
 app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
+    console.log("Hello World02a");
     res.json({message: "HELLO JSON"});
   } else {
+    console.log("Hello World02b");
     res.json({message: "Hello json"});
   };
   
 });
+console.log("Hello World03");
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html");
 });
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
