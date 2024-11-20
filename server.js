5ec0e1f9630ae60fc5bbff740b31cf4d10d4089e
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-20 19:42:18 -0300

    Change folder for the certificate.

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ if (!process.env.DISABLE_XORIGIN) {
 }
 
 const options = {
-  key: fs.readFileSync('/etc/tls/server.key'),
-  cert: fs.readFileSync('/etc/tls/server.crt')
+  key: fs.readFileSync('/opt/app-root/src/server.key'),
+  cert: fs.readFileSync('/opt/app-root/src/server.crt')
 }
 
 bGround.setupBackgroundApp(app, myApp, __dirname);
