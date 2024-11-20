38936f0e6d56159e03282b3a1d1d744d9f12630e
Eliandro Simim <eliandro.simim@gmail.com>
2024-11-20 14:41:56 -0300

    Change port number to 8080.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const options = {
 }
 
 bGround.setupBackgroundApp(app, myApp, __dirname);
-const PORT = 443;
+const PORT = 8080;
 const listener = https.createServer(options, app).listen(PORT, console.log(`Node.js listening on port  ${PORT}`))
 
 
